d6df68446f56a85dd21218e15f7540289ac9a839
Damian Ziemba <nazriel@dzfl.pl>
2024-04-12 22:53:39 +0200

    Response helper
    Signed-off-by: Damian Ziemba <nazriel@dzfl.pl>

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,66 @@ impl Request {
     }
 }
 
+struct Response<'a> {
+    body: String,
+    status: u16,
+    stream: &'a mut TcpStream,
+    headers: HashMap<String, String>,
+}
+impl<'a> Response<'a> {
+    fn new(stream: &'a mut TcpStream) -> Self {
+        Self {
+            body: String::new(),
+            status: 200,
+            stream,
+            headers: HashMap::new(),
+        }
+    }
+
+    fn status(&mut self, status: u16) -> &mut Self {
+        self.status = status;
+        self
+    }
+
+    fn body(&mut self, body: &str) -> &mut Self {
+        self.body = body.into();
+        self
+    }
+
+    fn header(&mut self, key: &str, value: &str) -> &mut Self {
+        self.headers.insert(key.into(), value.into());
+        self
+    }
+
+    async fn send(&mut self) -> anyhow::Result<()> {
+        let status_text = match self.status {
+            200 => "200 OK",
+            404 => "404 Not Found",
+            500 => "500 Internal Server Error",
+            _ => "500 Internal Server Error",
+        };
+        self.stream
+            .write_all(format!("HTTP/1.1 {status_text}\r\n").as_bytes())
+            .await?;
+
+        for (hname, hvalue) in &self.headers {
+            self.stream
+                .write_all(format!("{hname}: {hvalue}\r\n").as_bytes())
+                .await?;
+        }
+
+        if !self.body.is_empty() {
+            self.stream
+                .write_all(format!("Content-Length: {}\r\n\r\n", self.body.len()).as_bytes())
+                .await?;
+            self.stream.write_all(self.body.as_bytes()).await?;
+        } else {
+            self.stream.write_all(b"Content-Length: 0\r\n\r\n").await?;
+        }
+        Ok(())
+    }
+}
+
 async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
     println!("accepted new connection");
 
@@ -62,22 +122,21 @@ async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
     let req = Request::parse(&mut b).await?;
 
     if req.path == "/" {
-        stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await?;
+        Response::new(&mut stream).status(200).send().await?;
     } else if req.path.starts_with("/echo/") {
         let payload = req
             .path
             .strip_prefix("/echo/")
             .expect("some payload should exist");
 
-        stream.write_all(b"HTTP/1.1 200 OK\r\n").await?;
-        stream.write_all(b"Content-Type: text/plain\r\n").await?;
-        stream
-            .write_all(format!("Content-Length: {}\r\n\r\n", payload.len()).as_bytes())
+        Response::new(&mut stream)
+            .status(200)
+            .header("Content-Type", "text/plain")
+            .body(payload)
+            .send()
             .await?;
-
-        stream.write_all(payload.as_bytes()).await?;
     } else {
-        stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").await?;
+        Response::new(&mut stream).status(404).send().await?;
     }
 
     Ok(())
